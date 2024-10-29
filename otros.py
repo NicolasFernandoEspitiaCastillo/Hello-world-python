8ba37c7e083b38a54a0b0d51787b19d1e7bec123
NicolasFernandoEspitiaCastillo <186082646+NicolasFernandoEspitiaCastillo@users.noreply.github.com>
2024-10-29 12:44:10 -0500

    Tipos de variables de python

---
+++ otros.py
@@ -0,0 +1,9 @@
+fullname = input("Enter your full name: ")
+age = int(input("Enter your age "))
+gender = input("Enter your gender options M, F: ")
+heigth = float(input("Enter your height: "))
+
+print(f"""
+      Hola, usuario {fullname}. ¿Cómo estás? te saluda Python.
+      Gracias por compartir tu edad {age}; no sabia que eres un humano tipo {gender}.
+      """)
---
+++ tiposdevariables.py
@@ -0,0 +1,12 @@
+fullname = "Miguel Angel Castro Escamilla" #Tipo de dato - string, tipo de dato, es un conjunto de palabras (frase)
+gender = "M" #Tipo de datos char
+age = 22 #Tipo de dato int
+height = 1.68 # Tipo de dobule
+trainer = True #Tipo de dato boolean
+
+#Conjuntos
+
+hobbies = ["Programar", 'Cocinar', """Wow Horda"""] #Tipo de dato array - datos de conjunto
+direction = (dict) ["country": "Colombia", "state": "Santander"] #Tipo de dato object
+
+print (f"teacher's Name: {fullname}")
