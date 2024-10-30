2ba11f115d9023b0e56612dd08b582dde78b8a44
NicolasFernandoEspitiaCastillo <186082646+NicolasFernandoEspitiaCastillo@users.noreply.github.com>
2024-10-30 12:43:28 -0500

    operadores condicionales

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-name = input("enter your name: ")
-message = f"Hello World, I am {name}"
-print (message)
+# > Mayor
+# < Menor
+# => Mayor igual
+# <= Menos igual 
+# == igual
+# != Diferente
+# !== No Identico
+
+numero = 1
+numeroDos = "1"
+
+#print (numero !== numeroDos)
+
+#Estos operadores devuelven una afirmacion boleana si se cumplen o no "true" "false"-
